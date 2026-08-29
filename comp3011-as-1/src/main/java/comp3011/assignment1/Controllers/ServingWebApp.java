a8eb04321a3c69d4a20479dc0e01a35bc898936d
Vanessa-Soetjiadi-01 <vanessasoetjiadi@gmail.com>
2026-08-29 15:10:00 +0930

    Created index.html and can run webpage locally

---
+++ comp3011-as-1/src/main/java/comp3011/assignment1/Controllers/ServingWebApp.java
@@ -0,0 +1,11 @@
+package comp3011.assignment1.Controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebApp {
+	public static void main(String[] args) {
+        SpringApplication.run(ServingWebApp.class, args);
+    }
+}
---
+++ comp3011-as-1/src/test/java/comp3011/assignment1/Comp3011As1ApplicationTests.java
@@ -0,0 +1,13 @@
+package comp3011.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp3011As1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
